3164b8db11ad1981d1cfaa87b62ff79f768d62b4
Peter Ung <peter@Peters-MacBook-Pro.local>
2016-12-03 08:15:29 -0800

    Fix for post activity

--- app/src/main/java/com/peterung/redditzen/ui/post/PostActivity.java
+++ app/src/main/java/com/peterung/redditzen/ui/post/PostActivity.java
@@ -3,6 +3,7 @@ package com.peterung.redditzen.ui.post;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.peterung.redditzen.R;
 
@@ -27,6 +28,17 @@ public class PostActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
